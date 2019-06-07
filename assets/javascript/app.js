2b70e61311c64f05fe6b9fdc8531764c199d1c9c
ayoussefinia <alex.youssefinia@gmail.com>
2019-06-06 23:20:02 -0500

    finished game logic, still need to find trivia questions/answers andwork on scary picture zoom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -64,10 +64,17 @@ console.log(q1.correct == q1.optionB);
 let scaryImageArr = ["../images/Chuckie.jpg", "../images/freddy-kruger.jpg", "../images/it-the-clown.jpg", "../images/jason.jpg", "../images/jeepers-creepers.jpg", "../images/scream.jpg",];
 
 
-let seconds = 10;
-let ticket;
+let seconds = 15;
+let ticket; //clear interval
+let i = 0; // question index
+let Arr = questionArr; //scope fix
+let y; //clear timeout
+let nextQuestionTicket; //clear interval 
+let correctAnswers = 0;
+let incorrectAnswers = 0;
 
-//stop clock functions 
+
+//start the game with no clock, question or answer display
 $(".stop-clock").css("display","none");
 $(".question").css("display","none");
 $(".answer").css("display","none");
@@ -85,33 +92,79 @@ function stopClockGo() {
  ticket =  setInterval(secondCouter, 1000);
  
 }
-function stopClockStop() {
-  clearInterval(ticket);
-}
+
 function stopClockOperator() {
+  
   stopClockGo();
-  setTimeout(stopClockStop, 10000); 
+  y = setTimeout(stopClockStop, 15000); 
+  
+}
+function stopClockStop() {
+  
+  clearInterval(ticket);
+ 
+  clearTimeout(y);
 }
 
-function answerChecker(question) {
-  if(event.target.name == question.correct) {
+
+
+function answerChecker() {
+  stopClockStop();
+  console.log("answerchecker was run while i=" + i);
+  Arr = questionArr;
+  if(event.target.name == Arr[i].correct) {
+    correctAnswers ++;
     console.log("you chose the correct answer");
     $(".stop-clock").css("display","none");
     $(".question").css("display","none");
     $(".answer").css("display","none");
+    $("#main-button").css("display","none");
     $(".instruction-box").css("display","flex");
     $(".main-instructions").text("Correct");
     $(".stats").text("");
+    nextQuestionTimer();
   } else {
+    incorrectAnswers++;
     console.log("you chose the wrong answer");
     $(".stop-clock").css("display","none");
     $(".question").css("display","none");
     $(".answer").css("display","none");
+    $("#main-button").css("display","none");
     $(".instruction-box").css("display","flex");
     $(".main-instructions").text("Wrong");
-    $(".stats").text("The Correct Answer Was:" + " " + question.correct);
+    $(".stats").text("The Correct Answer Was:" + " " + questionArr[i].correct);
+    nextQuestionTimer();
   }
 }
+
+function nextQuestionTimer() {
+  console.log("next question timer was run while i=" +i);
+  console.log(Arr);
+  seconds = 15;
+  i++;
+  if(i < Arr.length) {
+    $(".stats-two").text(" ");
+    $("#stop-clock-text").text("15");
+    let nextQuestionTicket = setTimeout(function() {
+    
+      askQuestion(Arr[i]);
+      $(".instruction-box").css("display","none");
+    }, 2000);
+  }else if(i>= Arr.length){
+    stopClockStop();
+    $(".instruction-box").css("display","flex");
+    $(".main-instructions").text("Finished!!!");
+    $(".stats").text("Correct Answers: "+ correctAnswers);
+    $(".stats-two").text("Incorrect Answers: "+ incorrectAnswers);
+    $("#main-button").css("display","flex");
+    $("#main-button").text("Play Again");
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+  }
+  // return nextQuestionTicket;
+  // console.log("nqt:" + nextQuestionTicket);
+}
+
 function timeIsOut() {
   $(".instruction-box").css("display","flex");
   $(".stop-clock").css("display","none");
@@ -121,35 +174,49 @@ function timeIsOut() {
   $(".answer").css("display","none");
   $("#main-button").css("display","none");
   // setTimeout()
+  nextQuestionTimer();
 }
 
 // stopClockOperator();
 triviaQuestionAsked = false;
 gameStarted= false;
 
-function askQuestion(q) {
+function askQuestion() {
+  console.log("nextquestionticket:"+ nextQuestionTicket);
+  
+  console.log("hello from inside ask question i:"+ i);
+  console.log(questionArr[i]);
+  stopClockOperator();
+$(".stats-two").text("");
 $(".stop-clock").css("display","flex");
 $(".question").css("display","flex");
 $(".answer").css("display","flex");
-$(".question").text(q.question);
-$("#answer-text-a").text(q.optionA);
-$("#answer-text-b").text(q.optionB);
-$("#answer-text-c").text(q.optionC);
-$("#answer-text-d").text(q.optionD);
+$(".question").text(questionArr[i].question);
+// 
+$("#submit-button-a").text(questionArr[i].optionA);
+$("#submit-button-b").text(questionArr[i].optionB);
+$("#submit-button-c").text(questionArr[i].optionC);
+$("#submit-button-d").text(questionArr[i].optionD);
 }
 
 
 $("#game-container").click(function(event) {
 
 if(event.target.id == 'main-button'){
-  $(".instruction-box").css("display","none")
-  stopClockOperator();
-  askQuestion(q4);
+  $(".instruction-box").css("display","none"); 
+  if(i == questionArr.length ){
+    i=0;
+    $("#stop-clock-text").text("15");
+  }
+  console.log("the i above AQ:"+ i);
+  askQuestion();
 }
 
+
+
+
 if(event.target.classList.contains('submit-button')) {
-stopClockStop();
-answerChecker(q4);
+  answerChecker();
 };
 
 
